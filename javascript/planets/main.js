77377d0e97ff1f32d28264a149c539c33e4e7ee0
Sheel2007 <62717403+Sheel2007@users.noreply.github.com>
2023-03-23 14:08:17 -0400

    Delete javascript directory

--- javascript/planets/main.js
+++
@@ -1,6 +0,0 @@
-function updatePlanetStatus() {
-    let planet = document.getElementById("greenplanet");
-    planet.innerHTML = "<em>Red Alert:</em> hit by phaser fire!";
-}
-
-window.onload = updatePlanetStatus;
